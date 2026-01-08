b0e37f60b8ff8a8af27f84701e1469f68cda67c1
ivanap23 <pejovic.ivana23@gmail.com>
2026-01-08 16:48:48 +0100

    Implementirana Hero sekcija

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
     </div>
   );
 }
